13e4e1b32e884f00e0063bb058c028866a36b033
Lam Anh <buihuylam773@gmail.com>
2026-01-25 23:34:34 +0700

    demo2

---
+++ Assets/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float flySpeed = 5f;  // Tốc độ bay (có thể chỉnh Inspector)
+
+    void Update()
+    {
+        var newPosition = transform.position;
+        newPosition.y += Time.deltaTime * flySpeed;  // Bay lên (Y tăng)
+        transform.position = newPosition;
+    }
+}
---
+++ Assets/PlayerShooter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooter : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float shootingInterval = 0.2f;  // Khoảng cách bắn (giây)
+
+    private float lastBulletTime;  // Thời gian bắn lần cuối
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))  // GIỮ chuột trái
+        {
+            if (Time.time - lastBulletTime > shootingInterval)
+            {
+                ShootBullet();
+                lastBulletTime = Time.time;
+            }
+        }
+    }
+
+    private void ShootBullet()
+    {
+        Vector3 spawnPos = transform.position + new Vector3(0, 0.5f, 0);  // Offset mũi tàu
+        Instantiate(bulletPrefab, spawnPos, transform.rotation);
+    }
+}
